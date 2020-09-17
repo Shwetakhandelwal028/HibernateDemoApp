da9499e9e0b44a377ff44e0585aec4b5e89e3365
Shwetakhandelwal028 <shwetakhandelwal028@gmail.com>
2020-09-17 11:21:55 +0000

    added Hibernate demo app EMS

---
+++ src/main/java/com/shweta/hibernateDemoApp/HibernateDemoAppEMS/model/Employee.java
@@ -0,0 +1,36 @@
+package com.shweta.hibernateDemoApp.HibernateDemoAppEMS.model;
+
+public class Employee {
+	private int id;
+	private String empName;
+	private Double salary;
+	private String departement;
+	
+	
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public String getDepartement() {
+		return departement;
+	}
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}
+
+}
